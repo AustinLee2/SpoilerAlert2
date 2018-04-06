c196347e9f83d603a32e65b604a6766b52e16cc6
Austin Lee <austinhlee2@gmail.com>
2018-04-06 15:49:50 -0700

    added login functionality

---
+++ app/src/main/java/com/austinhlee/spoileralert/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.austinhlee.spoileralert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Choose authentication providers
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()))
+                .setIsSmartLockEnabled(false)
+                .build(), RC_SIGN_IN
+        );
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //updateUI(currentUser);
+    }
+
+
+
+}

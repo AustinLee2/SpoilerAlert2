406a506a95158177614b54fa8ee6a097f8412b82
jschliss <jschlisserma@oxy.edu>
2018-04-07 18:27:38 -0700

    Bottom Nav and Framework for Main Activity

--- app/src/main/java/com/austinhlee/spoileralert/MainActivity.java
+++ app/src/main/java/com/austinhlee/spoileralert/MainActivity.java
@@ -1,8 +1,13 @@
 package com.austinhlee.spoileralert;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent loginIntent = new Intent(this, LoginActivity.class);
         startActivity(loginIntent);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.home_item:
+                                selectedFragment = HomeFragment.newInstance();
+                                break;
+                            case R.id.contacts_item:
+                                selectedFragment = ContactsFragment.newInstance();
+                                break;
+                            case R.id.spoilers_item:
+                                selectedFragment = SpoilersFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment_container, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, HomeFragment.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
     }
 }

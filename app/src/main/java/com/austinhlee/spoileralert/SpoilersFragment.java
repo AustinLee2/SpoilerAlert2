2e6b6302c9d8c8ae376d6d9452c15ecf2464e9b0
jschliss <jschlisserma@oxy.edu>
2018-04-07 21:33:00 -0700

    Bottom Nav and Framework for Main Activity

---
+++ app/src/main/java/com/austinhlee/spoileralert/SpoilersFragment.java
@@ -0,0 +1,26 @@
+package com.austinhlee.spoileralert;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SpoilersFragment extends Fragment {
+
+    public static SpoilersFragment newInstance() {
+        SpoilersFragment fragment = new SpoilersFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_spoilers, container, false);
+    }
+}

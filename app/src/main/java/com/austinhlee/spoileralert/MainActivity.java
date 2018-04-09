7622a3ac721c871c96179023fc79eb9bab358f45
jschliss <jschlisserma@oxy.edu>
2018-04-08 18:19:30 -0700

    Bottom Nav and Framework for Main Activity

--- app/src/main/java/com/austinhlee/spoileralert/MainActivity.java
+++ app/src/main/java/com/austinhlee/spoileralert/MainActivity.java
@@ -5,18 +5,27 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent loginIntent = new Intent(this, LoginActivity.class);
-        startActivity(loginIntent);
+        startActivityForResult(loginIntent, RC_SIGN_IN);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
@@ -49,7 +58,26 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.fragment_container, HomeFragment.newInstance());
         transaction.commit();
 
-        //Used to select an item programmatically
-        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
     }
-}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            //succesfully signed in
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Already Signed In!", Toast.LENGTH_LONG).show();
+                return;
+            } else {
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No network connection!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+    }
+
+    }
+
